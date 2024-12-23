daf7913308f23eb70c47efbe51376e0566de3034
yunsuk <yunsukjeung@snu.ac.kr>
2024-12-23 21:19:25 +0900

    remove duck

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css'
-import { ducks } from './demo'
-import DuckItem from './DuckItem'
-
 
 function App() {
   console.log('start')
   return (
     <div>
       <h1>Reactivities</h1>
-      {ducks.map(duck=>(
-        <DuckItem key={duck.name} duck={duck}/>
-      ))}
     </div>
   )
 }
