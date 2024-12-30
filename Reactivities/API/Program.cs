357184dc55665eb00aebd1f37233c320a0d133aa
yunsuk <yunsukjeung@snu.ac.kr>
2024-12-30 19:00:34 +0900

    section3 end

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -4,8 +4,8 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,7 +13,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(opt =>
+{
+  opt.AddPolicy(
+    "CorsPolicy",
+    policy =>
+    {
+      policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    }
+  );
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +31,8 @@ if (app.Environment.IsDevelopment())
   app.UseSwagger();
   app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
@@ -41,8 +52,6 @@ catch (Exception ex)
   var logger = services.GetRequiredService<ILogger<Program>>();
   logger.LogError(ex, "An error occured during migration");
   throw;
-  
 }
-
 
 app.Run();
--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 function App() {
-  console.log('start')
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Reactivities</h1>
+      <Header as="h2" icon="user" content="Reacitvities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
