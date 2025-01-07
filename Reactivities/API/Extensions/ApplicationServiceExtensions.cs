6075003127361df2d335bd57fc490870b267cc44
yunsuk <yunsukjeung@snu.ac.kr>
2025-01-07 23:15:02 +0900

    end of section 4

--- Reactivities/API/Extensions/ApplicationServiceExtensions.cs
+++ Reactivities/API/Extensions/ApplicationServiceExtensions.cs
@@ -5,43 +5,38 @@ using Persistence;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtensions
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationServices(
+      this IServiceCollection services,
+      IConfiguration config
+    )
     {
-        public static IServiceCollection AddApplicationServices(
-            this IServiceCollection services,
-            IConfiguration config
-        )
-        {
-            // Learn more about configuring Swagger/OpenAPI at
-            // https://aka.ms/aspnetcore/swashbuckle
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy(
-                    "CorsPolicy",
-                    policy =>
-                    {
-                        policy
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .WithOrigins("http://localhost:3000");
-                    }
-                );
-            });
+      // Learn more about configuring Swagger/OpenAPI at
+      // https://aka.ms/aspnetcore/swashbuckle
+      services.AddEndpointsApiExplorer();
+      services.AddSwaggerGen();
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy(
+          "CorsPolicy",
+          policy =>
+          {
+            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+          }
+        );
+      });
 
-            services.AddMediatR(cfg =>
-                cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly)
-            );
+      services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
 
-            // services.AddMediatR(typeof(List.Handler).Assembly); // for
-            // mediatR 11
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-            return services;
-        }
+      // services.AddMediatR(typeof(List.Handler).Assembly); // for
+      // mediatR 11
+      services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+      return services;
     }
+  }
 }
